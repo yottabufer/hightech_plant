23f5049ffb2dae68d209a4cec2e0485f59e1c816
yottabufer <yottabufer@gmail.com>
2024-04-27 17:26:46 +0300

    startapp

---
+++ task/custom_user/models.py
@@ -0,0 +1,59 @@
+from uuid import uuid4
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Модель для Пользователя
+    """
+    uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False, unique=True, db_index=True)
+    email = models.EmailField(unique=True, verbose_name='email')
+
+    date_joined = models.DateTimeField(auto_now_add=True, verbose_name='Дата присоединения')
+    time_updated = models.DateTimeField(auto_now=True, verbose_name='время изменения')
+
+    is_active = models.BooleanField(default=True, verbose_name='Активный')
+    is_staff = models.BooleanField(default=False, verbose_name='Персонал')
+    is_superuser = models.BooleanField(default=False, verbose_name='Администратор')
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email} -- {self.uuid}'
+
+    @property
+    def is_admin(self):
+        return self.is_superuser or self.is_staff
+
+    class Meta:
+        default_permissions = ()
+        permissions = (
+            ('create_any_user', 'Создание любого пользователя'),
+            ('view_any_user', 'Просмотр информации любого пользователя'),
+            ('change_any_user', 'Изменение любого пользователя'),
+            ('delete_any_user', 'Удаление любого пользователя'),
+        )
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
---
+++ task/custom_user/serializers/user.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+
+    class Meta:
+        model = User
+        fields = ('email', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
